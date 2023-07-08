e8109a6c059d6af03e73b9c8aa1eb6dbf1c30d8c
Martin Doussinet <martin.doussinet@hei.yncrea.fr>
2023-07-08 13:33:34 +0200

    Part 2.3 : lists and conditions

--- la-maison-jungle/src/index.js
+++ la-maison-jungle/src/index.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import './styles/index.css';
-import App from './components/App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles/index.css'
+import App from './components/App'
 
-/*Old React => 17
-import ReactDOM from 'react-dom';
-ReactDOM.render(<React.StrictMode>
-  <App />
-</React.StrictMode>,document.getElementById('root'));
-*/
-
-/* New React => 18+*/
-import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
