97feb99cf17618a65d3cf4b75acfabbe5c4ebd69
Martin Doussinet <martin.doussinet@hei.yncrea.fr>
2023-07-20 13:46:25 +0200

    Part 2.5 : Using events to interact with components

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -2,6 +2,7 @@ import Banner from './Banner'
 import logo from '../assets/logo.png'
 // import Cart from './Cart'
 import ShoppingList from './ShoppingList'
+import Footer from './Footer'
 
 function App() {
 	return (
@@ -10,8 +11,10 @@ function App() {
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
+
 			{/* <Cart /> */}
 			<ShoppingList />
+			<Footer />
 		</div>
 	)
 }
